c945c0aac9050f4a18816ca8605135070db04f70
thania09 <tania_villalobos_93@hotmail.com>
2013-12-10 18:54:08 -0600

    practica5

---
+++ MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace convertidor2
+{
+	public partial class MainForm : Form
+	{
+		double pesos;
+		double dolares;
+		double tipcamb;
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(double.TryParse(textBox1.Text, out pesos) && double.TryParse(textBox3.Text, out tipcamb)
+					&& pesos!=0 && tipcamb!=0){
+
+					dolares = pesos/tipcamb;
+
+					textBox2.Text = dolares.ToString();
+		}
+
+			else{
+			MessageBox.Show("¡Error!!!!!!!! no llenaste los espacios\no ingresaste una letra");
+			}
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
